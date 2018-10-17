92ce1e024f7ad73b5a38a10dc08156c7c88cb2bc
VictoriousAura <shrreya.behll@hotmail.com>
2018-10-18 00:47:39 +0530

    BottomNav Added

---
+++ app/src/main/java/com/xyz/shrreya/bottonnavigationtablelayout/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.xyz.shrreya.bottonnavigationtablelayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import com.xyz.shrreya.bottonnavigationtablelayout.ui.fragment.HighlightsFragment
+import com.xyz.shrreya.bottonnavigationtablelayout.ui.fragment.Fragment_home
+import com.xyz.shrreya.bottonnavigationtablelayout.ui.fragment.Fragment_settings
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener= BottomNavigationView.OnNavigationItemSelectedListener {
+        item -> when(item.itemId){
+        R.id.menu_highlight->{
+            replaceFragment(HighlightsFragment())
+            return@OnNavigationItemSelectedListener true
+        }
+        R.id.menu_home->{
+            replaceFragment(Fragment_home())
+
+            return@OnNavigationItemSelectedListener true
+        }
+        R.id.menu_settings->{
+            replaceFragment(Fragment_settings())
+
+            return@OnNavigationItemSelectedListener true
+        }
+    }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        BottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        replaceFragment(Fragment_home())
+    }
+
+    private fun replaceFragment(fragment: Fragment)
+    {
+        val fragmentTransaction= supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentHolder,fragment)
+        fragmentTransaction.commit()
+    }
+}
---
+++ app/src/main/java/com/xyz/shrreya/bottonnavigationtablelayout/ui/fragment/Fragment_settings.kt
@@ -0,0 +1,19 @@
+package com.xyz.shrreya.bottonnavigationtablelayout.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.xyz.shrreya.bottonnavigationtablelayout.R
+
+
+class Fragment_settings : android.support.v4.app.Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_settings, container, false)
+    }
+
+
+}
